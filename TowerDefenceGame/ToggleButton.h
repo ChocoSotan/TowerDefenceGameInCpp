5ce47a029286df90c2a7f699bcc0a0412effab4a
keisuke maekawa <vclkgmnrr6@outlook.jp>
2018-08-24 14:24:03 +0900

    .hの内容を追加

--- TowerDefenceGame/ToggleButton.h
+++ TowerDefenceGame/ToggleButton.h
@@ -1,1 +1,15 @@
 #pragma once
+#include"Button.h"
+#include<vector>
+//#include<vector>
+class ToggleButton {
+public:
+	~ToggleButton() {}
+	ToggleButton() {
+
+	}
+	void update();
+	void addButton(int x, int y);
+private:
+	std::vector<Button*> button;
+};
