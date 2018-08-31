1e38eeae20d41c3cf99d4d4fec9c1fc14ed12399
keisuke maekawa <vclkgmnrr6@outlook.jp>
2018-08-31 23:30:04 +0900

    表示位置を変更しない場合のコンストラクタを追加

--- TowerDefenceGame/Gauge.h
+++ TowerDefenceGame/Gauge.h
@@ -5,13 +5,21 @@
 //表示する場所と呼び出し時点での数値を引数として受け取る。
 class Gauge {
 public:
-	Gauge(int sixe_x,int size_y,const int surfacecolor,const int backcolor,int difference_x,int difference_y) {
+	Gauge(int sixe_x,int size_y,const int &surfacecolor,const int &backcolor,int difference_x,int difference_y) {
 		this->size_x = size_x;
 		this->size_y = size_y;
 		this->surfacecolor = surfacecolor;
 		this->backcolor = backcolor;
 		this->difference_x = difference_x;
 		this->difference_y = difference_y;
+	}
+	Gauge(int size_x, int size_y, const int &surfacecolor, const int &baclcor) {
+		this->size_x = size_x;
+		this->size_y = size_y;
+		this->surfacecolor = surfacecolor;
+		this->backcolor = backcolor;
+		this->difference_x = 0;
+		this->difference_y = 0;
 	}
 	virtual~Gauge(){}
 	//二数を比較してpercentageを更新。
