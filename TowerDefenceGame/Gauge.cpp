f6bc891d6c68f5798d4d71964b0e0f0fd6de40af
keisuke maekawa <vclkgmnrr6@outlook.jp>
2018-08-31 23:26:23 +0900

    細部変更

--- TowerDefenceGame/Gauge.cpp
+++ TowerDefenceGame/Gauge.cpp
@@ -4,6 +4,6 @@ void Gauge::update(double num_max, double num_now) {
 	this->percentage = (int)(num_now / num_max * 100);
 }
 void Gauge::drawGauge(const Vector2D &position)const {
-	DrawBox(position.getX()+difference_x, position.getY()+difference_y, position.getX()+size_x+ difference_x, position.getY()+size_y+difference_y,GetColor(255,0,0),TRUE);
-	DrawBox(position.getX()+difference_x, position.getY()+difference_y, position.getX()+(size_x*percentage)/100+difference_x, position.getY()+size_y+difference_y, GetColor(0, 255, 0), TRUE);
+	DrawBox(position.getX()+this->difference_x, position.getY()+this->difference_y, position.getX()+this->size_x+ this->difference_x, position.getY()+this->size_y+this->difference_y,this->surfacecolor,TRUE);
+	DrawBox(position.getX()+this->difference_x, position.getY()+this->difference_y, position.getX()+(this->size_x*this->percentage)/100+this->difference_x, position.getY()+this->size_y+this->difference_y,this->backcolor, TRUE);
 }
